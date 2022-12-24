66530b28c961f4732eaabcf46ae3a4e03cf57617
Samuel <34555296+Flam3rboy@users.noreply.github.com>
2022-12-24 11:31:31 +0100

    perf: image optimization for static build

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
+const withExportImages = require("next-export-optimize-images");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
 	images: { unoptimized: false },
 };
 
-module.exports = nextConfig;
+module.exports = withExportImages(nextConfig);
