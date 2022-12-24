c001fd096923b719a821ab5c8d9e0e990b1364b9
Samuel <34555296+Flam3rboy@users.noreply.github.com>
2022-12-24 11:19:48 +0100

    feat: favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,24 +5,6 @@ import "../components/navbar.scss";
 import "../components/timeline.scss";
 import type { AppProps } from "next/app";
 
-import { Inter } from "@next/font/google";
-import Head from "next/head";
-
-const inter = Inter({ subsets: ["latin"], weight: ["900", "600", "400"] });
-
 export default function App({ Component, pageProps }: AppProps) {
-	return (
-		<>
-			<Head>
-				<meta name="viewport" content="width=device-width; initial-scale=1.0" />
-			</Head>
-			<style jsx global>{`
-				html {
-					font-family: ${inter.style.fontFamily};
-					// font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-				}
-			`}</style>
-			<Component {...pageProps} />
-		</>
-	);
+	return <Component {...pageProps} />;
 }
