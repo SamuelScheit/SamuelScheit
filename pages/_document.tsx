2c906dd3bd9b87d4a6c20db20f52b36ba9c78aa7
Samuel <34555296+Flam3rboy@users.noreply.github.com>
2023-02-16 20:30:10 +0100

    update og:image

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,9 @@ export default function Document() {
 				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
 				<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
 				<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+				<meta property="og:image" content="/media.png" />
+				<meta name="og:description" content="Samuel Scheit - Developer, Student, Founder" />
+				<meta property="og:locale" content="en_GB" />
 				<link rel="preload" crossOrigin="anonymous" type="font/woff2" as="font" href="/fonts/inter-v12-latin-900.woff2" />
 				<link rel="preload" crossOrigin="anonymous" type="font/woff2" as="font" href="/fonts/inter-v12-latin-600.woff2" />
 				<link rel="preload" crossOrigin="anonymous" type="font/woff2" as="font" href="/fonts/inter-v12-latin-regular.woff2" />
