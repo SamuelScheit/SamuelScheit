0d312953758eab41fee7fc676d686a48bed0abfe
samuelscheit <github@samuelscheit.com>
2023-12-02 13:35:49 +0100

    feat: add telegram

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import { Contact } from "../components/contact";
 import GitHub from "../public/github.svg";
 import Twitter from "../public/twitter.svg";
 import LinkedIn from "../public/linkedin.svg";
+import Telegram from "../public/telegram.svg";
 
 export const config = {
 	unstable_runtimeJS: false,
@@ -20,16 +21,27 @@ export default function Home() {
 				<meta name="description" content="Samuel Scheit - Developer, Student, Founder" />
 			</Head>
 			<div className="main">
-				<div className="links">
-					<a href="https://github.com/SamuelScheit/" target="_blank" rel="noreferrer">
-						<GitHub />
-					</a>
-					<a className="linkedin" href="https://www.linkedin.com/in/samuel-scheit-343436247/" target="_blank" rel="noreferrer">
-						<LinkedIn />
-					</a>
-					<a className="twitter" href="https://twitter.com/SamuelScheit" target="_blank" rel="noreferrer">
-						<Twitter />
-					</a>
+				<div className="links-wrapper">
+					<div className="links">
+						<a title="GitHub" href="https://github.com/SamuelScheit/" target="_blank" rel="noreferrer">
+							<GitHub />
+						</a>
+						<a
+							title="LinkedIn"
+							className="linkedin"
+							href="https://www.linkedin.com/in/samuel-scheit-343436247/"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<LinkedIn />
+						</a>
+						<a title="Twitter" className="twitter" href="https://twitter.com/SamuelScheit" target="_blank" rel="noreferrer">
+							<Twitter />
+						</a>
+						<a title="Telegram" className="telegram" href="https://t.me/SamuelScheit" target="_blank" rel="noreferrer">
+							<Telegram />
+						</a>
+					</div>
 				</div>
 				<div className="content">
 					<Hero />
