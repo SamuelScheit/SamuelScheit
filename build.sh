e23b23f5cd5e84dd15b2f8202a0343c185925251
Samuel Scheit <github@samuelscheit.com>
2024-06-10 16:33:21 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 docker build -t flam3rboy/private:samuelscheit .
 docker push flam3rboy/private:samuelscheit
 
-docker stack deploy -c docker-compose.yml samuelscheit --with-registry-auth
+# docker stack deploy -c docker-compose.yml samuelscheit --with-registry-auth
