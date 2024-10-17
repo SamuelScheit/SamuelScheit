efcf080c091f259677abb73ac3eef1b2202d3706
samuelscheit <github@samuelscheit.com>
2024-10-17 19:57:43 +0200

    fix: caching

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,6 @@ export default function Document() {
 	return (
 		<Html lang="en">
 			<Head>
-				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
 				<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
 				<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
 				<meta property="og:image" content="/media.png" />
